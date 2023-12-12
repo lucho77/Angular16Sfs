29e64c7eadd01746f62a71e5ed56f6e1207cf594
Bruggi07 <ruggiabruno07@gmail.com>
2023-12-12 19:07:51 -0300

    feat(menu-inicio): trae el menú y dibuja cada item

--- src/app/pages/home/menu-mobile/menu-mobile.component.ts
+++ src/app/pages/home/menu-mobile/menu-mobile.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu-mobile',
   templateUrl: './menu-mobile.component.html',
   styleUrls: ['./menu-mobile.component.scss']
 })
-export class MenuMobileComponent {
+export class MenuMobileComponent implements OnInit{
 
+  constructor(private router: Router){}
+  ngOnInit(): void {
+    this.getMenu();
+  }
+  menu: any;
+
+  public getMenu(): any{
+    this.menu = JSON.parse(localStorage.getItem('userMenu'));
+  }
+  public getRouter(ejecutar): any {
+    if (ejecutar != null) {
+      let url = 'pages/home/reportdef/' + ejecutar
+      this.router.navigate([url])
+    }
+  }
 }

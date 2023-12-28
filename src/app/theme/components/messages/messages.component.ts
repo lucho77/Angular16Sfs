858fd207baf6991361408179819b9afbf59aa486
Bruggi07 <ruggiabruno07@gmail.com>
2023-12-28 18:46:37 -0300

    feat(chat): filtros de busqueda

--- src/app/theme/components/messages/messages.component.ts
+++ src/app/theme/components/messages/messages.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 import { MessagesService } from './messages.service';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ChatComponent } from '../../chat/chat.component';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-messages',
@@ -16,13 +17,18 @@ export class MessagesComponent implements OnInit {
   public meetings: Array<any>;
   public msjsEnviados: Array<any>;
   public usuariosOn: Array<any>;
+  private usuariosArr: Array<any>;
   public userSearch: string;
   public toggleClassInput: boolean = false;
+  public usersFiltrados: Array<any> = [];
+  private userEncontrado : boolean;
+  @ViewChild('buscarNombre') inputBuscarNombre: ElementRef;
 
-  constructor(private messagesService: MessagesService, private modalService: NgbModal) {
+  constructor(private messagesService: MessagesService, private modalService: NgbModal, private aviso: ToastrService) {
     this.messages = messagesService.getMessages();
     this.meetings = messagesService.getMeetings();
     this.usuariosOn = messagesService.getUsuarios();
+    this.usuariosArr = messagesService.getUsuarios();
 
 
   }
@@ -38,11 +44,13 @@ export class MessagesComponent implements OnInit {
 
   }
 
-  abrirChat(idSession: number, nombre: string) {
+  abrirChat(idSession: number) {
+
+    if (!idSession) return
 
     let modalChat = this.modalService.open(ChatComponent, { size: 'lg', centered: true });
     modalChat.componentInstance.idSession = idSession;
-    modalChat.componentInstance.nombre = nombre;
+    modalChat.componentInstance.nombre = this.getNombreChatByIdSession(idSession);
     modalChat.componentInstance.messages = this.messages;
     modalChat.componentInstance.msjsEnviados = this.msjsEnviados;
 
@@ -50,13 +58,83 @@ export class MessagesComponent implements OnInit {
     return modalChat;
 
   }
+  getNombreChatByIdSession(idSession: number): any {
+    for (const user of this.usuariosOn) {
+      if (user.idSession === idSession) return user.name
+    }
+    this.aviso.error(`No se a encontrado ningun chat`);
+    return null
+  }
+
+  getIdSessionByName(nombre: string): number {
+
+    let nombreFiltrado = this.buscaUsers(nombre);
+    let nom: string;
+
+    if (nombreFiltrado != null) {
+      nom = nombreFiltrado.toUpperCase();
+    }
+    for (const user of this.usuariosArr) {
+      let nomU = user.name.toUpperCase();
+      if (nomU == nom) return user.idSession;
+    }
+    if (!this.userEncontrado) this.aviso.error(`No se encontró ningun chat con ${nombre}`);
+    return null;
+  }
+
   acortarText(texto: string): string {
     return texto.slice(0, 51) + '...';
   }
 
+
+
   clickClassEvent(): void {
     this.toggleClassInput = !this.toggleClassInput;
     this.userSearch = '';
+    this.elementoFocus();
   }
 
+  elementoFocus(): void {
+    if (this.toggleClassInput) {
+      setTimeout(() => {
+        this.inputBuscarNombre.nativeElement.focus();
+      }, 500);
+    }
+  }
+
+  buscaUsers(nombre: string): string {
+    if (!nombre) {
+      this.setListaUsuariosOriginal();
+      this.userEncontrado = true;
+      return
+    } else {
+      let nom = nombre.toUpperCase();
+      this.usuariosArr.forEach(user => {
+        let nomU = user.name.toUpperCase();
+        if (nomU.includes(nom)) {
+          this.usersFiltrados.push(user);
+        }
+      });
+
+      if (this.usersFiltrados.length == 1) {
+        let userEncontrado = this.usersFiltrados[0].name;
+        this.usersFiltrados = [];
+        this.userEncontrado = true;
+        return userEncontrado;
+      }
+      else if (this.usersFiltrados.length > 1) {
+        this.usuariosOn = this.usersFiltrados;
+        this.usersFiltrados = [];
+        this.userEncontrado = true;
+        return
+      }
+      else {
+        this.userEncontrado = false;
+        return
+      }
+    }
+  }
+  setListaUsuariosOriginal():void{
+    this.usuariosOn = this.usuariosArr;
+  }
 }
--- src/app/theme/components/messages/messages.service.ts
+++ src/app/theme/components/messages/messages.service.ts
@@ -5,6 +5,10 @@ export class MessagesService {
 
     private usuarios = [
         {
+            name: 'Gustavo',
+            idSession: 1334
+        },
+        {
             name: 'Ashley',
             idSession: 1111
         },
@@ -35,20 +39,36 @@ export class MessagesService {
         {
             name: 'Luciano',
             idSession: 1121
+        },
+        {
+            name: 'Luciano oroño',
+            idSession: 1124
+        },
+        {
+            name: 'Emiliano',
+            idSession: 1122
+        },
+        {
+            name: 'Gustavo L',
+            idSession: 1222
+        },
+        {
+            name: 'Gustavo S',
+            idSession: 1141
         }
     ]
     private messages = [
         {
             name: 'Ashley',
             text: 'After you get up and running, you can place Font Awesome icons just about...',
-            time: 1,
+            time: 3,
             enviado:false,
             idSession: 1111
         },
         {
             name: 'Ashley',
             text: 'Test Test Test Test Test Test Test Test Test Test Test Test Test Test ',
-            time: 1,
+            time: 2,
             enviado:false,
             idSession: 1111
         },
@@ -62,7 +82,7 @@ export class MessagesService {
         {
             name: 'Ashley',
             text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-            time: 2,
+            time: 1,
             enviado:false,
             idSession: 1111
         },
@@ -152,10 +172,31 @@ export class MessagesService {
         },
         {
             name: 'Luciano',
-            text: 'Hola muy buenas',
-            time: 0,
+            text: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for lorem ipsum will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).',
+            time: 1,
             enviado:false,
             idSession: 1121
+        },
+        {
+            name: 'Emiliano',
+            text: 'Hola muy buenas',
+            time: 5,
+            enviado:false,
+            idSession: 1122
+        },
+        {
+            name: 'Bruno',
+            text: 'Sos gay?',
+            time: 3,
+            enviado:true,
+            idSession: 1122
+        },
+        {
+            name: 'Emiliano',
+            text: 'Si, re gay',
+            time: 1,
+            enviado:false,
+            idSession: 1122
         }
     ];
 
